9ac2facecdef3db42bd9c4dceb80ed4b9e63704d
Thiago Barreto Reis <aprendizthiago@hotmail.com>
2023-10-20 03:36:25 -0300

    first creat

---
+++ src/main/java/me/dio/BradescoDev2023Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BradescoDev2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BradescoDev2023Application.class, args);
+	}
+
+}
